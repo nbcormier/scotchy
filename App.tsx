c65a73eba7b91442861fe90927bfa42f3059e9b3
Nick Cormier <nicklaus.cormier@macys.com>
2020-06-02 18:48:24 -0400

    chore(): initial commit, adding amplify

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx to start working on your app! Cool!</Text>
     </View>
   );
 }
